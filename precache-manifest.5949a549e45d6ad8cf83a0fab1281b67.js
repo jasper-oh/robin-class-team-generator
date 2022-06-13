59c7e5d357d0a364310de42fa7eb476db0d747ab
Jasper Oh <ohyj0906@gmail.com>
2022-06-12 23:08:13 -0700

    Updates

---
+++ precache-manifest.5949a549e45d6ad8cf83a0fab1281b67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "274067bb9e6cbc04f896",
+    "url": "/robin-class-team-generator/static/css/main.852a63f8.chunk.css"
+  },
+  {
+    "revision": "274067bb9e6cbc04f896",
+    "url": "/robin-class-team-generator/static/js/main.247d23e2.chunk.js"
+  },
+  {
+    "revision": "b3d659c50a44b3a87869",
+    "url": "/robin-class-team-generator/static/js/runtime~main.8d1147dd.js"
+  },
+  {
+    "revision": "7320f7fa9cfa6e34f70b",
+    "url": "/robin-class-team-generator/static/js/2.aeee0004.chunk.js"
+  },
+  {
+    "revision": "86b14bde71d00ccf44db5ee49ff453a3",
+    "url": "/robin-class-team-generator/index.html"
+  }
+];
